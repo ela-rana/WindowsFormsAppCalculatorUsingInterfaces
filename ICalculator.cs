5746f5ffbf2af5eadba0612aa1e06ff1945aa864
Ela Rana <elarana3@gmail.com>
2022-08-10 17:21:20 -0700

    calc

--- ICalculator.cs
+++ ICalculator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsAppCalculatorUsingInterfaces
 {
@@ -36,27 +37,50 @@ namespace WindowsFormsAppCalculatorUsingInterfaces
         }
         public static double Calculate(StringBuilder s)
         {
-            //STEP 1: ERROR HANDLING FOR DIVIDE BY ZERO WITHIN OUR STRING
+           //custom error handler for divide by zero
             DivideByZeroHandler(s);
 
-            //NOTE: Other errors are prevented by the button click event handlers 
-            //Here is a list of errors not allowed by the button click implementation by enabling/disabling certain button
-            //clicks after certain other button clicks are made
-            //1. two decimal points (like "3.2.2") for same number prevented by disabling '.'button after it is pressed until
-            //next operator or equal to sign is clicked
-            //2. two back to back operators (like "3/*") not allowed because operators are disabled after one operator is pressed
-            //and only reenables after another number or decimal point value is entered
-            //3. the calculation string cannot start or end with operator and cannot end with a decimal point
-            //Ex: these are not allowed: "/2+1", "2/3+", "2+5."
+            double result = 0;  //to hold the result of the calculation
 
-            //STEP 2: COUNT OPERATORS (and number of values being calculated will be number of operators + 1)
-            int operatorCount = CountOperators(s);
+            StringBuilder[] mathItem = new StringBuilder[5];    
+            //holds [num1],[operator1],[num2],[operator2],[num3]
+
+            int j=0;    //to hold the index location of mathItem
+            while(s.Length>0)
+            {
+                
+                
+                if ( !Regex.IsMatch(s[0].ToString(), @"[\+\-\/\*]") )   //if its part of the 
+                { 
+                    mathItem[j].Append(s[0]);
+                    s.Remove(0,1);
+                    if ( Regex.IsMatch(s[1].ToString(), @"[\+\-\/\*]") )    //if the next char is an operator
+                    {
+                        if(j==3 && Regex.IsMatch(mathItem[2].ToString(), @"[\/\*]"))
+                    }
+                }
+                //else    //if it is a math operator
+                //{
+                //    j++;
+                //    mathItem[j].Append(s[0]);
+                //    s.Remove(0,1);
+                //    j++;
+                //    if(j==)
+               // }
+
+            }
 
 
-            double result = 0;
 
-            //Nullable<double>[] values = new Nullable<double>[20];
-            return result;
+
+
+
+
+
+
+
+
+            return result;  //to return the result of the calculation
         }
         public static void DivideByZeroHandler(StringBuilder s)
         {
@@ -93,16 +117,36 @@ namespace WindowsFormsAppCalculatorUsingInterfaces
                     keepChecking = false;   //to stop while loop
                 }
             }
-        }
-        public static int CountOperators(StringBuilder s)
-        {
-            int count = 0;
-            foreach (char c in s.ToString())
+
+            StringBuilder[] items = new StringBuilder[5];
+            int itemPosition = 0;
+            for(int i=0; i<s.Length; i++)
             {
-                if (c == '+' || c == '-' || c == '*' || c == '/')
-                    count++;
+                if (items[i]==)
             }
-            return count;
         }
     }
 }
+
+        //Didn't need the following method
+        //public static int CountOperators(StringBuilder s)
+        //{
+        //    int count = 0;
+        //    foreach (char c in s.ToString())
+        //    {
+        //        if (c == '+' || c == '-' || c == '*' || c == '/')
+        //            count++;
+        //    }
+        //    return count;
+        //}
+        //int operatorCount = CountOperators(s);
+
+//NOTE: Other errors are prevented by the button click event handlers 
+//Here is a list of errors not allowed by the button click implementation by enabling/disabling certain button
+//clicks after certain other button clicks are made
+//1. two decimal points (like "3.2.2") for same number prevented by disabling '.'button after it is pressed until
+//next operator or equal to sign is clicked
+//2. two back to back operators (like "3/*") not allowed because operators are disabled after one operator is pressed
+//and only reenables after another number or decimal point value is entered
+//3. the calculation string cannot start or end with operator and cannot end with a decimal point
+//Ex: these are not allowed: "/2+1", "2/3+", "2+5."
